f8cf550eab965c4d475b8699f156f6da56264fbd
Vivek Tarnallya <116621113+vivekbeast@users.noreply.github.com>
2023-12-10 17:18:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,249 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import './index.css';
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  GlobeAltIcon,
+  CursorArrowRaysIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+  ShoppingCartIcon,
+  
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon} from '@heroicons/react/20/solid'
+import Lottie from "lottie-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import wel from './WEL.json'
+
+
+
+
+const products = [
+  
+  { name: 'Shopping Site', description: 'Explore my clothing store.', href: 'https://vivekbeast.github.io/vt-5/index.html', icon: ShoppingCartIcon },
+  { name: 'Directionless Science', description: 'The greatest project & content.', href: 'https://vivekbeast.github.io/Dscience/', icon: CursorArrowRaysIcon },
+  { name: 'Weather App', description: 'Using API, this site shows weather details.', href: 'https://vivekbeast.github.io/Weather-QuoteAPP/', icon: GlobeAltIcon },
+  { name: 'Integrations', description: 'All-in-one WeBPAGE of all my projects.', href: 'https://vivekbeast.github.io/UP2/', icon: SquaresPlusIcon },
+  { name: 'My First Webpage', description: "Let's time-travel back.", href: 'https://vivekbeast.github.io/Project4/', icon: ArrowPathIcon },
+
+
+]
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default App;
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  
+  return (
+    <header  className=" bg-inherit fixed top-0 w-full shadow-lg z-10 rounded-lg">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex flex-row lg:flex-1">
+        <Lottie className=' h-7 align-top' animationData={wel}></Lottie>
+        </div>
+        
+        <div className="flex lg:hidden">
+        
+          <button
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: 10, opacity: 0 }}
+          transition={{ duration: 0.6 }}
+
+
+
+
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-slate-100"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-7 w-7 rounded-md" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12 ">
+        
+          
+          
+          <h1 href="#" className="text-sm font-semibold leading-6 text-slate-200">
+            Home
+          </h1>
+          <Popover className="relative ">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-200">
+              Git Projects
+              <ChevronDownIcon className="h-5 w-5 flex-none text-slate-200" aria-hidden="true" />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-300"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-gradient-to-b from-gray-200 via-slate-300 to-gray-200 shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-gray-900">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+          <h1 href="#" className="text-sm font-semibold leading-6 text-slate-200">
+            Contact
+          </h1>
+          <h1 href="#" className="text-sm font-semibold leading-6 text-slate-200">
+            About
+          </h1>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <h1  className="text-sm font-semibold leading-6 text-slate-200">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </h1>
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden " open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gradient-to-b from-gray-800 via-slate-500 to-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <h1 href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              {/* <img
+                className="h-8 w-auto rounded-full"
+                src={iron}
+                alt=""
+              /> */}
+            </h1>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6 text-slate-100" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-slate-200 hover:bg-gray-50">
+                        Git Projects
+                        <ChevronDownIcon
+                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="flex rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray hover:bg-gray-50  items-center"
+                          >
+                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg  group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-100 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>{item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <h1
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-slate-200 hover:bg-gray-50"
+                >
+                  Home
+                </h1>
+                <h1
+                  href="https://github.com/vivekbeast"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-slate-200 hover:bg-gray-50"
+                >
+                  Contact
+                </h1>
+                <h1
+                  href="https://www.google.com/search?client=firefox-b-d&q=human"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-slate-200 hover:bg-gray-50"
+                >
+                  About
+                </h1>
+              </div>
+              <div className="py-6">
+                <h1 
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-slate-200 hover:bg-gray-50"
+                >
+                  Log in <span aria-hidden="true">&rarr;</span>
+                </h1>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
+
---
+++ src/Land.js
@@ -0,0 +1,141 @@
+import React from 'react';
+// import as from './abstract-minimalism-4k-gz.jpg'
+// import {motion} from "framer-motion"
+import Lottie from "lottie-react";
+import hi from './ROBOT.json'
+import iron from './IRONMAN.png'
+import {motion} from "framer-motion"
+import {Popover } from '@headlessui/react'
+import {
+    ArrowPathIcon,
+    GlobeAltIcon,
+    CursorArrowRaysIcon,
+    SquaresPlusIcon,
+    ShoppingCartIcon,
+  } from '@heroicons/react/24/outline'
+  import { IoLogoCss3 } from "react-icons/io5";
+  import { IoLogoHtml5 } from "react-icons/io5";
+  import { SiTailwindcss } from "react-icons/si";
+  import { IoLogoJavascript } from "react-icons/io5";
+  import { FaReact } from "react-icons/fa";
+
+
+
+
+
+
+
+
+const products = [
+  
+    { name: 'Shopping Site', description: 'Explore my clothing store.', href: 'https://vivekbeast.github.io/vt-5/index.html', icon: ShoppingCartIcon },
+    { name: 'Directionless Science', description: 'The greatest project & content.', href: 'https://vivekbeast.github.io/Dscience/', icon: CursorArrowRaysIcon },
+    { name: 'Weather App', description: 'Using API, this site shows weather details.', href: 'https://vivekbeast.github.io/Weather-QuoteAPP/', icon: GlobeAltIcon },
+    { name: 'Integrations', description: 'All-in-one WeBPAGE of all my projects.', href: 'https://vivekbeast.github.io/UP2/', icon: SquaresPlusIcon },
+    { name: 'My First Webpage', description: "Let's time-travel back.", href: 'https://vivekbeast.github.io/Project4/', icon: ArrowPathIcon },
+  
+  
+]
+
+export default function Land(){
+    return(
+        <div className=' bg-gradient-to-b from-gray-800 via-slate-500 to-gray-800 static'>
+          
+            <section className='h-screen flex flex-col justify-center items-center '>
+            <motion.div 
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 100, opacity: 0 }}
+                transition={{ duration: 0.6 }}  
+                // -m-1.5 p-1.5
+             className=" justify-center text-center -m-.5 p-2.5 mx-auto max-w-lg ">
+<h1 className="text-3xl font-bold bg-gradient-to-r from-green-300 via-yellow-300 to-pink-400 p-4 rounded-md text-transparent bg-clip-text">
+          knowlEDGE is the EDGE of Universe
+        </h1>          
+          </motion.div>
+            {/* <Lottie className=' justify-center' animationData={hi}></Lottie> */}
+            <motion.div 
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 10, opacity: 0 }}
+                transition={{ duration: 0.6 }}  
+                  
+             className="-m-1.5 p-1.5">
+            <img className="w-auto h-48 rounded-full" src={iron} alt="" />
+            
+          </motion.div>
+          <motion.div  
+            
+            
+            initial={{ y: 300, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -300, opacity: 0 }}
+            
+            
+            
+            className='-m-.5 p-3.5 font-semibold text-xl  text-slate-100'><h1 className=' text-3xl'>Hi, I’m <a href='./' class="underline decoration-emerald-500 decoration-4">Vivek Tarnallya</a></h1><p className=' font-mono text-xl'>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p></motion.div>
+            {/* <Lottie animationData={iii}></Lottie> */}
+            
+            </section>
+            <section className='h-screen flex flex-col'>
+            <div className=' mt-8'>
+            <Lottie className=' h-52 align-top' animationData={hi}></Lottie></div>
+            <div className=' justify-center text-center -m-.5 p-2.5 mx-auto max-w-lg'>
+            <h1 className=' justify-center text-2xl font-sans text-slate-50 text-center -m-.5 p-2.5 '> Projects</h1>
+                <Popover>
+                <div className="p-2 bg-slate-800 text-left rounded-xl">
+                
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-base leading-6 hover:bg-slate-500"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg  group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-slate-100 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-slate-100">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-400">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                
+             </Popover>
+            </div>
+            </section>
+            <section className=''>
+                <div className='  justify-center text-center -m-.5 p-2.5 mx-auto max-w-lg'>
+                <h1 className=' justify-center text-2xl font-sans text-slate-50 text-center -m-.5 p-2.5'>What i Know...</h1>
+                <div className="flex flex-wrap justify-center gap-4 bg-slate-800 p-5 rounded-md">
+  <div className=" w-24 text-center bg-gray-200 p-4 rounded-md flex flex-row align-center justify-center">
+    <h1>React</h1>
+    <FaReact />
+  </div>
+  <div className=" w-26 text-center bg-gray-200 p-4 rounded-md flex flex-row align-center justify-center">
+    <h1>Javascript</h1>
+    <IoLogoJavascript/>
+  </div>
+  <div className=" w-26 text-center bg-gray-200 p-4 rounded-md flex flex-row align-center justify-center">
+    <h1>Tailwind</h1>
+    <SiTailwindcss />
+  </div>
+  <div className=" w-22 text-center bg-gray-200 p-4 rounded-md flex flex-row align-center justify-center">
+    <h1>HTML</h1>
+    <IoLogoHtml5 />
+  </div>
+  <div className=" w-22 text-center bg-gray-200 p-4 rounded-md flex flex-row align-center justify-center">
+    <h1>CSS</h1>
+    <IoLogoCss3 />
+  </div>
+</div>
+
+                </div>
+            </section>
+        </div>
+
+    )
+}
