265d35282eae9ee3df0487bcf0143b561f203303
Vivek Tarnallya <116621113+vivekbeast@users.noreply.github.com>
2024-04-14 20:41:23 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import Land from './land.js';
+import Hand from './land.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Land />
+    <Hand />
   </React.StrictMode>,
   document.getElementById('root')
 );
